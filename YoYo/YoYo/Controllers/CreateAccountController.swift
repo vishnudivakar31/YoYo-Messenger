d65ea45be2d39df31ce78801f559bd4fefd3b0d0
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-02-07 17:36:12 -0500

    create account storyboard

--- YoYo/YoYo/Controllers/CreateAccountController.swift
+++ YoYo/YoYo/Controllers/CreateAccountController.swift
@@ -8,7 +8,32 @@
 import UIKit
 
 class CreateAccountController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var dobPicker: UIDatePicker!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        confirmPasswordTextField.delegate = self
+        dobPicker.maximumDate = Calendar.current.date(byAdding: .year, value: -14, to: Date())
+    }
+    
+    @IBAction func createButtonTapped(_ sender: Any) {
+    }
+    
+}
+
+extension CreateAccountController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
