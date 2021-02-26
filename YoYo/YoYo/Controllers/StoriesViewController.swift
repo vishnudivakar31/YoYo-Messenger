b7e58a4d0acbdf9c4ce9e9093b19faf429bb39f1
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-02-25 19:23:12 -0500

    video 60 seconds constraint

--- YoYo/YoYo/Controllers/StoriesViewController.swift
+++ YoYo/YoYo/Controllers/StoriesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class StoriesViewController: UIViewController {
 
@@ -36,6 +37,13 @@ class StoriesViewController: UIViewController {
         myStoryStackView.layer.addSublayer(bottomBorder)
     }
     
+    private func presentAlert(title: String, msg: String) {
+        let alertController = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func showMyStoriesTapped(_ sender: Any) {
     }
     
@@ -51,6 +59,26 @@ class StoriesViewController: UIViewController {
 extension StoriesViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         // TODO:- Handle media
-        
+        var videoLengthConstraintMet = true
+        let mediaType = info[.mediaType]
+        if mediaType != nil && mediaType as! String == "public.image" {
+            print("image")
+        } else if mediaType != nil && mediaType as! String == "public.movie" {
+            print("movie")
+            let videoURL = info[.mediaURL] as? URL
+            if let videoURL = videoURL {
+                let asset = AVURLAsset(url: videoURL)
+                let duration = asset.duration.seconds
+                if duration > 60 {
+                    videoLengthConstraintMet = false
+                } else {
+                    // TODO:- Save video to firestore
+                }
+            }
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        if !videoLengthConstraintMet {
+            presentAlert(title: "Story uploading failed", msg: "Story should be less than 60 seconds.")
+        }
     }
 }
